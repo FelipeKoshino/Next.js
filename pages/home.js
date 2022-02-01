3ba0acb74e8c8782ac8e63e977c0992c196e0d9a
Felipe Koshino <felipekoshino@gmail.com>
2022-02-01 13:35:28 -0300

    made changes

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,15 @@
+import Link from 'next/link';
+//As otimizações da tag "Link" não ocorrem no ambiente local, mas no ambiente de produção
+//otimizações para página dinãmica funcionar como uma single-page estática
+
 function Home() {
   return(
     <>
+    <Link ref="/portifolio">
     <h1>Esta é a Homepage do site</h1>
     <h2>Feel free to navigate</h2>
     <p>Parágrafo único da página principal</p>
+    </Link>
     </>
   );
 }
