c4ead8df7feb6831bee6ccc43e5a3bdf0a919e44
Felipe Koshino <felipekoshino@gmail.com>
2022-02-01 09:25:08 -0300

    atualizacao

--- pages/portifolio.js
+++ pages/portifolio.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 function Portifolio() {
   return(
     <>
-    <h1>Esta é a pagina de PORTIFÓLIO</h1>
-    <h2>Feel free to navigate</h2>
-    <p>Parágrafo único</p>
+    <Link ref="/portifolio">
+      <h1>Esta é a pagina de PORTIFÓLIO</h1>
+      <h2>Feel free to navigate</h2>
+      <p>Parágrafo único</p>
+    </Link>
     </>);
 }
 
